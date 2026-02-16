59813001ba13898e42ff00e0d745e3c62915bc7e
JoseMalcaR <jmalca@utem.cl>
2026-02-16 01:06:08 -0300

    Moving the project to MVC.

---
+++ Router.php
@@ -0,0 +1,43 @@
+<?php 
+
+namespace MVC;
+
+class Router{
+
+    public $rutasGET = [];
+    public $rutasPOST = [];
+
+    public function get($url, $fn){
+        $this->rutasGET[$url] = $fn;
+    }
+
+
+    public function comprobarRutas(){
+        $urlActual = $_SERVER['PATH_INFO'] ?? '/';
+        $metodo = $_SERVER['REQUEST_METHOD'];
+
+        if($metodo === 'GET'){
+            $fn = $this->rutasGET[$urlActual] ?? null;
+        }
+        if($fn){
+            // La url existe y hay una función asociada
+            call_user_func($fn, $this);
+        } else {
+            echo "Pagina no encontrada";
+        }
+    }
+    //Muestra una vista
+    public function render($view, $datos = []){
+
+        foreach($datos as $key => $value){
+            $$key = $value; // Crea una variable con el nombre de la clave y le asigna el valor
+        }
+
+        ob_start(); // Almacena en memoria lo que se imprime
+        extract($datos);
+        include __DIR__ . "/views/$view.php";
+        $contenido = ob_get_clean(); // Obtiene el contenido almacenado en memoria y lo limpia
+        
+        include __DIR__ . "/views/layout.php";
+    }
+}
---
+++ controllers/PropiedadController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Controllers;
+use MVC\Router;
+use Model\Propiedad;
+
+
+class PropiedadController{
+
+    public static function index(Router $router){
+
+        $propiedades = Propiedad::all();
+        $resultado =  null;
+    
+        $router->render('propiedades/admin', [
+            'propiedades' => $propiedades,
+            'resultado' => $resultado
+        ]);
+    }
+
+    public static function crear(){
+        echo "Desde el controlador de propiedades - crear";
+    }
+
+    public static function actualizar(){
+        echo "Desde el controlador de propiedades - actualizar";
+    }
+    
+}
